7ff11384cbd94414cdd31e16fb71132640158aee
unknown <rafal.mierzejewski.it@gmail.com>
2024-04-05 15:34:09 +0200

    added enemies spawn, enemy attacking, player health

--- classes/Enemy.lua
+++ classes/Enemy.lua
@@ -1,13 +1,15 @@
 Enemy = Moveable:extend()
 
-function Enemy:new(x, y, hp, speed)
-    Enemy.super.new(self, x, y, hp)
+function Enemy:new(x, y, hp, speed, damage)
+    Enemy.super.new(self, x, y, hp, speed)
     self.image = love.graphics.newImage("static/images/E.png")
     self.width = self.image:getWidth()
     self.height = self.image:getHeight()
-    self.speed = speed
     self.angle = math.atan2(Player:getY() - self.y, Player:getX() - self.x)
     self.radius = self.image:getWidth() / 2
+    self.damage = damage
+    self.rate_of_fire = 1
+    self.rate_of_fire_timer = 0
 end
 
 function Enemy:update(dt)
@@ -16,12 +18,17 @@ function Enemy:update(dt)
     local cos = math.cos(self.angle)
     local sin = math.sin(self.angle)
     local distance = GetDistance(self.x, self.y, Player:getX(), Player:getY())
+    if self.rate_of_fire_timer > 0 then
+        self.rate_of_fire_timer = self.rate_of_fire_timer - dt
+    end
     if distance > 55 then
         self.x = self.x + self.speed * cos * dt
         self.y = self.y + self.speed * sin * dt
+    elseif self.rate_of_fire_timer <= 0 then
+        Player:takeDamage(self.damage)
+        self.rate_of_fire_timer = self.rate_of_fire
     end
-
-    if self.hp <= 0 then
+    if self.current_hp <= 0 then
         self.delete = true
     end
 end
